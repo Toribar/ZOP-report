aa660bd9888441d2ee658eaba5fcd80ddae0bfc1
Nemanja <nemanja.d.krstic@gmail.com>
2016-05-03 18:18:44 +0200

    carbon locale, reports create button

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -4,13 +4,17 @@
     <div class="container">
         <div class="panel panel-primary">
             <div class="panel-heading">
+                <a class="btn btn-success btn-sm pull-right" href="{{ route('reports.create') }}">
+                    <i class="fa fa-plus"></i> Novi izveštaj
+                </a>
+
                 <h4>Izveštaji</h4>
             </div>
 
             <div class="list-group">
                 @forelse ($reports as $report)
                     <a class="list-group-item" href="{{ route('reports.show', $report->id) }}">
-                        <small class="pull-right">{{ $report->created_at->format('d.m.Y \u H:i:s') }}</small>
+                        <small class="pull-right">{{ $report->user->name }}, {{ $report->created_at->diffForHumans() }}</small>
                         <h4 class="list-group-item-heading">{{ $report->object ?: '(prazno)' }}</h4>
                         <p class="list-group-item-text">{{ $report->institution_number ?: '(prazno)' }}</p>
                     </a>
